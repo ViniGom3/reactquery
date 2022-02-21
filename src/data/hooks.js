8d3ebd7008909302e8526ea1c4e869fb087d9cab
Lucas Vinicius <vinicius.gom3@gmail.com>
2022-02-21 14:06:38 -0300

    idk

--- src/data/hooks.js
+++ src/data/hooks.js
@@ -11,16 +11,6 @@ const defaultOptions = {
   staleTime: 10000,
 };
 
-export const defaultValues = {};
-
-// const useQueryUser = (options) => {
-//   const queryKey = options?.queryKey || defaultQueryKey;
-//   const fetcher = options?.fetcher || defaultFetcher;
-//   const option = options?.option || defaultOptions;
-
-//   return useQuery(queryKey, fetcher, option);
-// };
-
 const useQueryUser = ({
   queryKey = defaultQueryKey,
   fetcher = defaultFetcher,
@@ -30,29 +20,3 @@ const useQueryUser = ({
 };
 
 export default useQueryUser;
-
-// import { useQuery } from "react-query";
-// import axios from "axios";
-
-// const defaultOptions = {
-//   staleTime: 5000,
-// };
-
-// const useQueryUser = ({
-//   queryKey = defaultQueryKey,
-//   fetcher = defaultFetcher,
-//   options = defaultOptions,
-// }) => {
-//   return useQuery(queryKey, fetcher, options);
-// };
-
-// const getUserById = async (userId) => {
-//   const { data } = await axios.get(
-//     `https://jsonplaceholder.typicode.com/users/${userId}`
-//   );
-//   return data;
-// };
-
-// export default function useQueryUser(userId) {
-//   return useQuery(["user", userId], () => getUserById(userId));
-// }
