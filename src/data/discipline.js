0020cd77815d7af44eb821c7d36ea662dff2c5ec
Lucas Vinicius <vinicius.gom3@gmail.com>
2022-02-22 18:49:26 -0300

    some fixes

--- src/data/discipline.js
+++ src/data/discipline.js
@@ -1,13 +1,13 @@
 import { api, CustomUseQuery, CustomUseMutation } from ".";
 
-const findAll = () => CustomUseQuery("Alldisciplines", api.discipline.findAll);
+const findAll = () => CustomUseQuery("allDisciplinas", api.discipline.findAll);
 
 // useQuery
 const findOne = (id) =>
-  CustomUseQuery("Discipline", api.discipline.findOne(id));
+  CustomUseQuery(`disciplina - ${id}`, api.discipline.findOne(id));
 
 // useMutates
-const add = () => CustomUseMutation(api.discipline.add, "Alldisciplines");
+const add = () => CustomUseMutation(api.discipline.add, "allDisciplinas");
 const deleteOne = () =>
   CustomUseMutation(api.discipline.deleteOne, "Discipline");
 
